a5a439d97fb358a18c16c777c56e08982d2be45c
adnan <adnaninci86@gmail.com>
2021-01-28 11:14:37 -0600

    commit for test branch

--- src/test/java/com/nextBaseCRM/tests/adnan/TestBranch.java
+++ src/test/java/com/nextBaseCRM/tests/adnan/TestBranch.java
@@ -5,5 +5,7 @@ public class TestBranch {
     public static void main(String[] args) {
 
         System.out.println("hello team!");
+
+        System.out.println("why there is a conflict?");
     }
 }
