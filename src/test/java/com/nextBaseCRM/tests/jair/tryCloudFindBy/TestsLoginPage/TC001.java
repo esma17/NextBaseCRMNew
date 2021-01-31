f6aa14eb7f601151ef7d04adc5639c0bf8d14d67
Jair Novello <jaja505@hotmail.com>
2021-01-31 03:26:46 -0500

    Add Branch TC001

---
+++ src/test/java/com/nextBaseCRM/tests/jair/tryCloudFindBy/TestsLoginPage/TC001.java
@@ -0,0 +1,7 @@
+package com.nextBaseCRM.tests.jair.tryCloudFindBy.TestsLoginPage;
+
+public class TC001 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
