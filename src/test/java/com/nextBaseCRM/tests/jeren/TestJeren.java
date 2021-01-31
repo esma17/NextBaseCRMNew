af7aef1f6a135a6d27389d61e384ed5dfa269319
Jeren <jerenshamuhamedova@gmail.com>
2021-01-30 23:28:45 -0600

    comment added

--- src/test/java/com/nextBaseCRM/tests/jeren/TestJeren.java
+++ src/test/java/com/nextBaseCRM/tests/jeren/TestJeren.java
@@ -3,5 +3,7 @@ package com.nextBaseCRM.tests.jeren;
 public class TestJeren {
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        // i like my classmates
     }
 }
